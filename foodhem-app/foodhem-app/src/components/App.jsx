8be10b3eb6f2971d027e86e7c263e815b4c30430
Zubair-droid <syedzubair3693@gmail.com>
2020-12-07 09:14:59 +0530

    firstcommit

---
+++ foodhem-app/foodhem-app/src/components/App.jsx
@@ -0,0 +1,14 @@
+//jshint esversion:6
+import React from "react";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello </h1>
+      <h6>Lets start</h6>
+    </div>
+  );
+}
+
+export default App;
---
+++ foodhem-app/src/App.js
@@ -0,0 +1,7 @@
+//jshint esversion:6
+import React from 'react';
+function App() {
+  
+}
+
+export default App;
